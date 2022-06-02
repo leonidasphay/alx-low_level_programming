1c3f261215f60360165d460ffb9db616fc5c34ba
Favour <favourkolawole21@gmail.com>
2022-06-02 13:34:54 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.>
+
+/**
+ * main - print the string in the puts function
+ *
+ * Description: using main function this program prints
+ * "Programming is like building a multilingual puzzle
+ * Return: 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
