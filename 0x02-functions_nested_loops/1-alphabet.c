fe9940031df06e0a67e29dc57b31128388361454
Favour <favourkolawole21@gmail.com>
2022-06-08 08:40:24 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ *
+*/
+void print_alphabet(void)
+{
+	int 1;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar (i);
+	}
+	_putchar ('\n');
+}
+
