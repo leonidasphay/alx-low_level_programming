9e6833156d546c8728a6632db1bc448c3b0769da
Favour <favourkolawole21@gmail.com>
2022-06-08 06:33:41 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,9 @@
  *
  * Return: On success 1.
  *
- * On error, -1 is returned, and errno is set appropriately.
- *
 */
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return (putchar(1));
 }
