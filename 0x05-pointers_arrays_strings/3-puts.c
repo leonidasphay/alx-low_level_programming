d7ea3703d3ea88e2a84903bc307b7b4bf0d6c676
Favour <favourkolawole21@gmail.com>
2022-06-13 08:21:36 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints string with puts
+ *
+ * @str: char str input
+ *
+ * Return: Always 0
+*/
+
+void _puts(char *str)
+{
+	puts(str);
+}
