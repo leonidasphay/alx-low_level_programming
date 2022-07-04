f31fa2b37136a71a96763f4986fa2625edfe5834
Favour <favourkolawole21@gmail.com>
2022-07-04 22:39:31 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - Contains neccesary data of a identificate a dog
+ * @name: name of the dog.
+ * @age: age of a dog.
+ * @owner: ownwer of a dog.
+ */
 
-#include <stdio.h>
-#include <stdlib.h>
 struct dog
 {
+
 	char *name;
 	float age;
 	char *owner;
 };
+
+
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
