ce56114bfc0cd43a8323d9b783e0ea27ab85a442
Favour <favourkolawole21@gmail.com>
2022-07-04 22:20:13 +0100

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+  *print_dog - prints various outcomes
+  *@d: variable to structure
+  */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+{
+return;
+}
+(d->name == NULL) ? printf("Name: (nil)\n")
+		: printf("Name: %s\n", d->name);
+
+		printf("Age: %f\n", d->age);
+
+		(d->owner == NULL) ? printf("Owner: (nil)\n")
+		: printf("Owner: %s\n", d->owner);
+
+}
