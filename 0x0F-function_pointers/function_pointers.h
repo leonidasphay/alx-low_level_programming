3e868cff42cd8341038cca3ae4a2bb55f1d21c53
Favour <favourkolawole21@gmail.com>
2022-07-06 18:43:51 +0100

    0-print_name.c

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef P_FUNCTIONS
+#define P_FUNCTIONS
+int int_index(int *array, int size, int (*cmp)(int));
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+#endif
