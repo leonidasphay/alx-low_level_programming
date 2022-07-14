41e0a5bea802f1071efb156369465f83a3adeef8
Favour <favourkolawole21@gmail.com>
2022-07-14 21:23:38 +0100

    singly linked

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists"
+
+/**
+ * _100first - Prints a message before main function
+ **/
+
+void __attribute__ ((constructor)) _100first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
