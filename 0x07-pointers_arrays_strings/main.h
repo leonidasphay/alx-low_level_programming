33458f60b49e2b88253eab317c2c8941fcdd900c
leonidasphay <102064909+leonidasphay@users.noreply.github.com>
2022-07-14 22:12:00 +0100

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #ifndef MAIN_H
 
 #define MAIN_H
@@ -24,15 +24,5 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
 #endif /*MAIN_H*/
-=======
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
->>>>>>> b75065fee8fe53cdcc90b5d3f44b6ac47735d1ef
+
+
