e134523050a41ba64a9de3bc89368fe831933f41
Favour <favourkolawole21@gmail.com>
2022-09-06 19:35:25 +0100

    def

--- 0x18-dynamic_libraries/6-abs.c
+++ 0x18-dynamic_libraries/6-abs.c
@@ -4,24 +4,13 @@
 /**
  * _abs - computes
  * Description: computes the absolute value of an integer
- *
- * Return: 0
- * @n:input
  */
 
 int _abs(int n)
 {
-if (n > 0)
-{
-n = n;
-}
-else if (n < 0)
-{
-n = -n;
-}
-else if (n == 0)
-{
-n = 0;
-}
-return (n);
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
 }
